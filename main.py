5928d9a5b65a9580c757f17b786c82386eaa4e1f
Butusen <j.moran3@usp.ceu.es>
2023-03-16 19:01:31 +0100

    carga de datos

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+df_train = pd.read_csv("sales_train.csv")
+df_shops = pd.read_csv("shops-translated.csv")
+df_items = pd.read_csv("item_category.csv")
+df_calendar = pd.read_csv("calendar.csv")
