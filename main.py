a2234addc9e27bda504be4d0700a0bef8a9bdd53
Butusen <j.moran3@usp.ceu.es>
2023-05-28 11:39:28 +0200

    gráficos retocados bien subido

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
@@ -27,12 +28,15 @@ df_final = pd.merge(df_nuevo, df_items, on="item_id", how="left")
 
 df_bo2 = df_final[df_final['item_id']== 2308]
 #df_bo2 = df_bo2[ df_final['City'] == "Moscow"]
-
 df_bo2_transformado = df_bo2[['date', 'item_cnt_day']]
 df_bo2_transformado['date'] = pd.to_datetime(df_bo2_transformado['date'])
 weeks=df_bo2_transformado.groupby([pd.Grouper(key='date', freq='W')])['item_cnt_day'].sum()
 df_weeks_bo2 = pd.DataFrame(weeks)
 df_weeks_bo2.plot()
+plt.title("Unidades vendidas de Call of Duty:Black Ops 2(PC Jewel)")
+plt.xlabel("Fecha")
+plt.ylabel("Cantidad")
+plt.show()
 df_bo2_train = df_weeks_bo2.iloc[0:104]
 df_bo2_test = df_weeks_bo2.iloc[104:148]
 df_bo2_train.plot()
@@ -45,11 +49,9 @@ estacionaria_bo2.plot(color="red")
 estacionaria_bo2_test.plot(color ="blue")
 #Tiene una tenedencia repetida anualmente
 acf = plot_acf(df_bo2_train)
-#pacf = plot_pacf(df_bo2_train)
+pacf = plot_pacf(df_bo2_train)
 dftest = adfuller(df_bo2_train, autolag='AIC')
 ## Es estacionaria porque el p-valor < 0.05, es 2.88 x 10^-6
-hola = auto_arima(df_bo2_train, trace=True, suppress_warnings=True)
-hola.summary()
 
 def checkings(df,df2,p,d,q,p2,d2,q2):
     fit_arima = ARIMA(df, order=(p,d,q),seasonal_order=(p2, d2, q2, 52))
@@ -57,7 +59,11 @@ def checkings(df,df2,p,d,q,p2,d2,q2):
     fit_arima2.summary()
     predict = fit_arima2.predict(start=105,end=147).to_frame()
     comparacion = predict.join(df2)
-    plt.plot(comparacion)
+    ax = comparacion.plot(y=['item_cnt_day', 'predicted_mean'])
+    ax.legend(['datos reales', 'datos pronosticados'])
+    ax.set_title("Comparación datos reales y pronosticados 2015")
+    ax.set_xlabel("Fecha")
+    ax.set_ylabel("Nº de unidades")
     plt.show()
     mse = mean_squared_error(comparacion['item_cnt_day'], comparacion['predicted_mean'])
     r_2 = r2_score(comparacion['item_cnt_day'], comparacion['predicted_mean'])
@@ -84,16 +90,20 @@ checkings(df_bo2_train,df_bo2_test,0,2,2,1,0,0)
 prueba_total = auto_arima(df_weeks_bo2, trace=True, suppress_warnings=True)
 prueba_total.summary()
 
-weekly_dates = pd.date_range(start="2015-11-08", end="2016-12-31", freq='W')
-weekly_df = pd.DataFrame(index=weekly_dates)
-df_cuatro_años= pd.concat([df_weeks_bo2,weekly_df])
-
 fit_arima_real = ARIMA(df_weeks_bo2, order=(1,1,0),seasonal_order=(1, 0, 0, 52))
 fit_arima2_real = fit_arima_real.fit()
 fit_arima2_real.summary()
 predict = fit_arima2_real.predict(start=147,end=207).to_frame()
-plt.plot(df_weeks_bo2)
-plt.plot(predict)
+plt.plot(df_weeks_bo2,label="datos reales")
+plt.plot(predict, label="datos pronosticados")
 plt.xticks(rotation='vertical')
+plt.xlabel("Fecha")
+plt.ylabel("Nº de unidades vendidas")
+plt.legend()
 plt.show()
 
+result = seasonal_decompose(df_weeks_bo2['item_cnt_day'], model='multiplicative')
+
+result.seasonal.plot()
+plt.title("Gráfico de estacionalidad")
+plt.show()
