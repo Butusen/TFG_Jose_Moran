dbc1d782a8f489808dd8faa02d72c748404ab092
Butusen <j.moran3@usp.ceu.es>
2023-06-29 11:11:57 +0200

    Comentarios añadidos

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
-
+# Lectura de los archivos
 df_train = pd.read_csv("sales_train.csv")
 df_shops = pd.read_csv("shops-translated.csv")
 df_items = pd.read_csv("item_category.csv")
 df_calendar = pd.read_csv("calendar.csv")
 df_censo = pd.read_csv("censoRusia.csv",sep=";")
 df_shops = pd.merge(df_shops,df_censo, on="City", how="left")
+
+# Conversión a formato fecha
 df_train['date'] = pd.to_datetime(df_train['date'], format="%d.%m.%Y")
 df_calendar['date'] = pd.to_datetime(df_calendar['date'])
 
+# Unión de las tablas en una única
 df_nuevo = pd.merge(df_train, df_shops, on='shop_id', how="left")
 df_final = pd.merge(df_nuevo, df_items, on="item_id", how="left")
 
-
+# Selección de un producto y agrupación de ventas semanalmente
 df_bo2 = df_final[df_final['item_id']== 2308]
-#df_bo2 = df_bo2[ df_final['City'] == "Moscow"]
 df_bo2_transformado = df_bo2[['date', 'item_cnt_day']]
 df_bo2_transformado['date'] = pd.to_datetime(df_bo2_transformado['date'])
 weeks=df_bo2_transformado.groupby([pd.Grouper(key='date', freq='W')])['item_cnt_day'].sum()
@@ -34,6 +36,8 @@ plt.title("Unidades vendidas de Call of Duty:Black Ops 2(PC Jewel)")
 plt.xlabel("Fecha")
 plt.ylabel("Cantidad")
 plt.show()
+
+# Datos de entrenamiento y test
 df_bo2_train = df_weeks_bo2.iloc[0:104]
 df_bo2_test = df_weeks_bo2.iloc[104:148]
 
@@ -41,14 +45,15 @@ log_bo2 = np.log(df_bo2_train).plot()
 plt.title("Datos de entrenamiento aplicando logaritmo")
 plt.xlabel("Fecha")
 plt.show()
-#Con el log comprobamos si es estable en varianza, no nos sirve
+# Con el log comprobamos si es estable en varianza, no nos sirve
 
+# Comprobación de estacionalidad
 result = seasonal_decompose(df_weeks_bo2['item_cnt_day'], model='multiplicative')
-
 result.seasonal.plot()
 plt.title("Gráfico de estacionalidad")
 plt.show()
 
+# Comprobación de estacionariedad
 estacionaria_bo2 = df_bo2_train.diff()
 estacionaria_bo2_test = df_bo2_test.diff()
 plt.plot(estacionaria_bo2,label="train",color="red")
@@ -59,13 +64,36 @@ plt.xlabel("Fecha")
 plt.ylabel("Diferencia")
 plt.legend()
 plt.show()
-#Tiene una tenedencia repetida anualmente
+# Tiene una tenedencia repetida anualmente
 acf = plot_acf(df_bo2_train)
 pacf = plot_pacf(df_bo2_train)
 dftest = adfuller(df_bo2_train, autolag='AIC')
 ## Es estacionaria porque el p-valor < 0.05, es 2.88 x 10^-6
 
 def checkings(df,df2,p,d,q,p2,d2,q2):
+    """
+    Perform ARIMA modeling and prediction on the given dataframes.
+
+    Args:
+        df (Dataframe): Training data.
+        df2 (Dataframe): Test data.
+        p (int): Autoregressive order of ARIMA.
+        d (int): Differencing order of ARIMA.
+        q (int): Moving average order of ARIMA.
+        p2 (int): Autoregressive order of seasonal ARIMA.
+        d2 (int): Differencing order of seasonal ARIMA.
+        q2 (int): Moving average order of seasonal ARIMA.
+
+    Returns:
+        tsa.arima.model.ARIMAResultsWrapper: Results and statistics of the model.
+        int: Mean Squared Error (MSE) of the prediction.
+
+    Examples:
+        Example usage of the function:
+        >>> checkings(df_bo2_train, df_bo2_test, 1, 0, 1, 1, 1, 0)
+        (ARIMAResultsWrapper, 501.923)
+
+    """
     fit_arima = ARIMA(df, order=(p,d,q),seasonal_order=(p2, d2, q2, 52))
     fit_arima2 = fit_arima.fit()
     fit_arima2.summary()
@@ -78,10 +106,13 @@ def checkings(df,df2,p,d,q,p2,d2,q2):
     ax.set_ylabel("Nº de unidades")
     plt.show()
     mse = mean_squared_error(comparacion['item_cnt_day'], comparacion['predicted_mean'])
-    r_2 = r2_score(comparacion['item_cnt_day'], comparacion['predicted_mean'])
     print(fit_arima2.summary())
     print("MSE="+str(mse))
-    
+
+# Primera aproximación
+checkings(df_bo2_train,df_bo2_test,1,0,1,1,1,0) 
+
+# Prueba de otros parámetros
 checkings(df_bo2_train,df_bo2_test,1,0,0,1,0,0)
 checkings(df_bo2_train,df_bo2_test,1,0,0,1,1,0)
 checkings(df_bo2_train,df_bo2_test,1,0,0,1,0,1)
@@ -90,13 +121,12 @@ checkings(df_bo2_train,df_bo2_test,1,0,0,0,1,0)
 checkings(df_bo2_train,df_bo2_test,1,0,0,2,0,0)
 checkings(df_bo2_train,df_bo2_test,1,1,0,1,0,0)
 checkings(df_bo2_train,df_bo2_test,1,0,1,1,0,0)
-checkings(df_bo2_train,df_bo2_test,1,0,1,1,1,0)
 checkings(df_bo2_train,df_bo2_test,1,0,1,1,1,1)
 checkings(df_bo2_train,df_bo2_test,2,1,0,1,0,0)
 checkings(df_bo2_train,df_bo2_test,2,1,1,1,0,0)
 
 
-#Ahora probamos con todos los datos
+#Ahora se realiza la predicción con todos los datos
 
 fit_arima_real = ARIMA(df_weeks_bo2, order=(1,1,0),seasonal_order=(1, 0, 0, 52))
 fit_arima2_real = fit_arima_real.fit()
